261a6e720d6c6eb49149db88048e6e7a75c97c3a
R-Stanz <stanz@tutamail.com>
2023-09-15 11:51:41 -0300

    C07 - Basic 05 - Simple For Loop

---
+++ basic_java/05-for_loop_simple/README.txt
@@ -0,0 +1,2 @@
+You should make a program capable of printing in
+just one line all numbers between 10 and 20.
---
+++ basic_java/05-for_loop_simple/possible_answer/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+
+		for(Integer i = 11; i < 20; i++){
+			System.out.print(i + " "); 	// Prints everything on
+						   	// the same line
+		}
+		System.out.println();			// Prints an empty line
+	}
+}
